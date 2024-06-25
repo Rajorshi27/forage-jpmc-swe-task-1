f7508a506ad8e54c384b142f0a7172aa0a9506a7
Rajorshi27 <64450538+Rajorshi27@users.noreply.github.com>
2024-06-25 12:43:40 -0400

    Update client_test.py

--- client_test.py
+++ client_test.py
@@ -7,19 +7,49 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+    
+    for quote in quotes:
+            self.assertEqual(
+                getDataPoint(quote),
+                (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], 
+                 (quote['top_bid']['price'] + quote['top_ask']['price']) / 2)
+            )
 
+  
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+
+    for quote in quotes:
+            self.assertEqual(
+                getDataPoint(quote),
+                (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], 
+                 (quote['top_bid']['price'] + quote['top_ask']['price']) / 2)
+            )
 
 
-  """ ------------ Add more unit tests ------------ """
-
-
+  def test_getRatio(self):
+        # Test when price_b is not zero
+        price_a = 120.0
+        price_b = 60.0
+        self.assertEqual(getRatio(price_a, price_b), 2.0)
+        
+        # Test when price_b is zero
+        price_a = 120.0
+        price_b = 0.0
+        self.assertIsNone(getRatio(price_a, price_b))
+        
+        # Test when price_a is zero
+        price_a = 0.0
+        price_b = 120.0
+        self.assertEqual(getRatio(price_a, price_b), 0.0)
+        
+        # Test when both prices are zero
+        price_a = 0.0
+        price_b = 0.0
+        self.assertIsNone(getRatio(price_a, price_b))
 
 if __name__ == '__main__':
     unittest.main()
